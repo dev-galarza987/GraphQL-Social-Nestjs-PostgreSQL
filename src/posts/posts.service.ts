f1e42882f8fdc59e0c9cefe02b134f4c78aede5c
Victor H. Galarza J. <dev.galarza@gmail.com>
2026-09-16 22:42:17 -0400

    refactor(posts): removing code base default

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
@@ -8,8 +9,34 @@ export class PostsService {
     return 'This action adds a new post';
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  findAll(): Post[] {
+    return [
+      {
+        id: 1,
+        title: "NestJS 12",
+        description: "Descripción sobre NestJS 12"
+      },
+      {
+        id: 2,
+        title: "ASP.NET Core 8",
+        description: "Descripción sobre ASP.NET Core 8."
+      },
+      {
+        id: 3,
+        title: "Spring Boot 7",
+        description: "Descripción sobre Spring Boot 7"
+      },
+      {
+        id: 4,
+        title: "FastAPI",
+        description: "Descripción sobre FastAPI"
+      },
+      {
+        id: 5,
+        title: "Axum",
+        description: "Descripción sobre Axum."
+      }
+    ];
   }
 
   findOne(id: number) {
