a18636fb2b4a4fde8a4cb5b91e04cfe4c4652725
Victor H. Galarza J. <dev.galarza@gmail.com>
2026-09-17 17:46:54 -0400

    feat(posts): add modeling orm
    Se modela la entidad Post con TypeORM

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,13 +1,18 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType({ description: "Entidad GraphQL de publicaciones." })
+@Entity({ name: "post" })
 export class Post {
   @Field(() => Int, { description: 'Identificador unico de publicación.' })
+  @PrimaryGeneratedColumn({ name: "post_id", type: 'int' })
   id: number;
 
   @Field({ nullable: false, description: "Título de la publicación." })
+  @Column({ type: 'varchar', length: 255, nullable: false })
   title: string;
 
   @Field({ description: "Description opcional de la publicación." })
+  @Column({ type: 'text', nullable: true })
   description?: string;
 }
