5ace1a4e783c7e67ebd6f152c152ee82fbbfe9b6
Victor H. Galarza J. <dev.galarza@gmail.com>
2026-09-16 22:41:50 -0400

    refactor(posts): removing code base default

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,7 +1,13 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
-@ObjectType()
+@ObjectType({ description: "Entidad GraphQL de publicaciones." })
 export class Post {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int, { description: 'Identificador unico de publicación.' })
+  id: number;
+
+  @Field({ nullable: false, description: "Título de la publicación." })
+  title: string;
+
+  @Field({ description: "Description opcional de la publicación." })
+  description?: string;
 }
