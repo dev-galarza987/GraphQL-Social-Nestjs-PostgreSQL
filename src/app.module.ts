21c6f8b3d6f5b1d939ac7489d2081b190c65dab1
Victor H. Galarza J. <dev.galarza@gmail.com>
2026-09-17 17:51:12 -0400

    refactor(app): add configuration of TypeOrm
    Se agrega la configuracion de persistencia de datos a la aplicacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,23 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'node:path'
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  }), PostsModule],
+  }), PostsModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'GqlSocialDB',
+    username: 'galarza',
+    password: '12413087',
+    synchronize: true,
+    autoLoadEntities: true,
+    entities: [__dirname + '/**/*.entity{.ts,.js}']
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
