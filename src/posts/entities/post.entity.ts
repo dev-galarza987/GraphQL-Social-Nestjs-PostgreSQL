e48b24a1ec0525c20ca16ba2ff91642b01ce14eb
Victor H. Galarza J. <dev.galarza@gmail.com>
2026-09-22 17:40:22 -0400

    refactor(posts): add refactoring to entity posts

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,18 +1,27 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { IEntity } from '../../shared/entity/generic.entity';
 
-@ObjectType({ description: "Entidad GraphQL de publicaciones." })
-@Entity({ name: "post" })
-export class Post {
-  @Field(() => Int, { description: 'Identificador unico de publicación.' })
-  @PrimaryGeneratedColumn({ name: "post_id", type: 'int' })
+@ObjectType({ description: 'Entidad GraphQL de publicaciones.' })
+@Entity({ name: 'post' })
+export class Post implements IEntity {
+  @Field(() => Int, { description: 'Identificador único de publicación.' })
+  @PrimaryGeneratedColumn({ name: 'post_id', type: 'int' })
   id: number;
 
-  @Field({ nullable: false, description: "Título de la publicación." })
+  @Field({ nullable: false, description: 'Título de la publicación.' })
   @Column({ type: 'varchar', length: 255, nullable: false })
   title: string;
 
-  @Field({ description: "Description opcional de la publicación." })
+  @Field({ nullable: true, description: 'Descripción opcional de la publicación.' })
   @Column({ type: 'text', nullable: true })
   description?: string;
+
+  @Field(() => Date, { description: 'Fecha de creación de la publicación.' })
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field(() => Date, { description: 'Fecha de última actualización.' })
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
