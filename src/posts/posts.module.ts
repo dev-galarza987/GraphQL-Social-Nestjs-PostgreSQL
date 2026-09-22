2f688025c44cfb96a3b4df00d912dd51045be3dc
Victor H. Galarza J. <dev.galarza@gmail.com>
2026-09-22 17:44:44 -0400

    refactor(posts): refactoring of posts module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PostsService } from './posts.service';
-import { PostsResolver } from './posts.resolver';
+import { PostsService } from './services/posts.service';
+import { PostsResolver } from './resolvers/posts.resolver';
+import { PostsRepository } from './repositories/posts.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
-  providers: [PostsResolver, PostsService],
+  providers: [PostsResolver, PostsService, PostsRepository],
+  exports: [PostsService, PostsRepository],
 })
-export class PostsModule { }
+export class PostsModule {}
